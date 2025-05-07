be8d58fbe0e94c16b0c46f011b25ba9decf045db
Adiyat Wira Negara <kazutonine@gmail.com>
2025-05-07 20:57:58 +0700

    Fix SvelteKit assets path configuration

--- svelte.config.js
+++ svelte.config.js
@@ -19,8 +19,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/pomodoro' : '',
-			assets: process.env.NODE_ENV === 'production' ? '/pomodoro' : ''
+			base: process.env.NODE_ENV === 'production' ? '/pomodoro' : ''
 		},
 		appDir: 'app'
 	}
